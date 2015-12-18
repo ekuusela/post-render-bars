a469205d0fd7c5a0bcfb35d9f6e5a7c5d06006b1
Eero Kuusela <eero.kuusela@gmail.com>
2015-12-18 14:54:21 +0200

    use class for finding elements instead of data-attributes for a significant performance improvement

--- watch.js
+++ watch.js
@@ -21,42 +21,79 @@
 }(this, function () {
     'use strict';
 
-    var id = 0;
+    var idCounter = 0;
     var attributeName = 'data-watch-id';
 
     /**
      * Starts watching the body for addition of a Node described by the given HTML string.
      *
-     * Modifies the HTML to have an identifying attribute and returns that. When that attribute is encountered, the callback is invoked.
+     * Modifies the first element in the HTML to have a class attribute or appends a new class if it already has one.
      *
-     * The callback gets a single argument: the first element defined in the template.
+     * When an element with the class is encountered, the class list is restored/removed and the callback is invoked.
+     *
+     * Returns the modified HTML.
+     *
+     * The callback gets the first element as an argument.
      */
     function forHtml(html, callback) {
-        html = html.trim();
-        var index = Math.min(html.indexOf(' '), html.indexOf('>'));
-        if (index === -1) {
-            return html;
-        }
+        idCounter += 1;
+        var className = '_watched_' + idCounter;
+        var result = appendToClass(html, className);
+        if (!result) { return html; }
+        html = result;
 
-        id += 1;
-        html = html.slice(0, index) + ' ' + attributeName + '="' + id + '"' + html.slice(index);
-
-        var onMutate = function(id) {
+        var onMutate = function() {
             return function() {
-                var elements = document.querySelectorAll('[' + attributeName + '="' + id + '"]');
-                var element;
+                var elements = document.getElementsByClassName(className);
                 if (elements.length > 0) {
-                        observer.disconnect();
-                    element = elements[0];
-                    element.removeAttribute(attributeName);
-                    callback(element);
+                    observer.disconnect();
+                    for (var i = elements.length - 1; i >= 0; i--) {
+                        var element = elements[i];
+                        removeClass(element, className);
+                        callback(element);
                     }
-                };
+                }
+            };
         };
 
-        var observer = new MutationObserver(onMutate(id));
+        var observer = new MutationObserver(onMutate());
         observer.observe(document.body, {childList: true, subtree: true});
         return html;
+    }
+
+    function appendToClass(html, className) {
+        html = html.trim();
+        var attributeStr = ' class="';
+        var attributeIndex = html.indexOf(attributeStr);
+        var closingIndex = html.indexOf('>');
+        if (closingIndex === -1) { return; }
+
+        var hasClass = attributeIndex > -1 && attributeIndex < closingIndex;
+        if (hasClass) {
+            var classIndex = html.indexOf('"', attributeIndex + attributeStr.length);
+            html = insert(html, classIndex, ' ' + className);
+        } else {
+            var spaceIndex = html.indexOf(' ');
+            var newClassIndex;
+            if (spaceIndex === -1) {
+                newClassIndex = closingIndex;
+            } else {
+                newClassIndex = Math.min(closingIndex, spaceIndex);
+            }
+            html = insert(html, newClassIndex, ' class="' + className +'"');
+        }
+        return html;
+    }
+
+    function removeClass(element, className) {
+        element.classList.remove(className);
+        if (element.classList.length === 0) {
+            element.removeAttribute('class');
+        }
+    }
+
+    function insert(str, index, item) {
+        return str.slice(0, index) + item + str.slice(index);
     }
 
     return {
