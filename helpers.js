e0628ff0bfa3e41d15f9a90285e2e5b08a3557e3
Eero Kuusela <eero.kuusela@gmail.com>
2016-01-21 00:57:03 +0200

    add missing dep

--- helpers.js
+++ helpers.js
@@ -6,17 +6,17 @@
  (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['handlebars', 'watch'], factory);
+        define(['handlebars', 'watch', 'parseHtml'], factory);
     } else if (typeof exports === 'object') {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
-        module.exports = factory(require('handlebars', 'watch'));
+        module.exports = factory(require('handlebars', 'watch', 'parseHtml'));
     } else {
         // Browser globals (root is window)
-        root.helpers = factory(root.Handlebars, root.watch);
+        root.helpers = factory(root.Handlebars, root.watch, root.parseHtml);
     }
-}(this, function(Handlebars, watch) {
+}(this, function(Handlebars, watch, parseHtml) {
     'use strict';
 
    /**
